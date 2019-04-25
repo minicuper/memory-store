ecab64e967f9682572e47ff3d9250c41ba38d25e
Artur Zayats <zag2art@gmail.com>
2019-04-25 16:29:02 +0300

    get back to old js

--- index.js
+++ index.js
@@ -12,40 +12,38 @@ const promisify = (syncFn) => {
   })
 }
 
-class MemoryStore {
-  constructor () {
+function MemoryStore() {
+  this.memory = {}
+}
+
+MemoryStore.prototype.getItem = function (key) {
+  return promisify(() => {
+    return this.memory[key]
+  })
+}
+
+MemoryStore.prototype.setItem = function (key, value) {
+  return promisify(() => {
+    this.memory[key] = value
+  })
+}
+
+MemoryStore.prototype.removeItem = function (key) {
+  return promisify(() => {
+    delete this.memory[key]
+  })
+}
+
+MemoryStore.prototype.getItemIds = function () {
+  return promisify(() => {
+    return Object.keys(this.memory)
+  })
+}
+
+MemoryStore.prototype.clean = function () {
+  return promisify(() => {
     this.memory = {}
-  }
-
-  getItem = (key) => {
-    return promisify(() => {
-      return this.memory[key]
-    })
-  }
-
-  setItem = (key, value) => {
-    return promisify(() => {
-      this.memory[key] = value
-    })
-  }
-
-  removeItem = (key) => {
-    return promisify(() => {
-      delete this.memory[key]
-    })
-  }
-
-  getItemIds = () => {
-    return promisify(() => {
-      return Object.keys(this.memory)
-    })
-  }
-
-  clean = () => {
-    return promisify(() => {
-      this.memory = {}
-    })
-  }
+  })
 }
 
 module.exports = MemoryStore
